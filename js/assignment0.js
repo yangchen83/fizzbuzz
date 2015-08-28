74aac219aff20cff3e90f742a7a12833d3fb6e86
Yang Chen <yang1983chen@gmail.com>
2015-08-28 17:32:58 -0400

    changed output to the webpage instead of console

--- js/assignment0.js
+++ js/assignment0.js
@@ -1,17 +1,27 @@
+$(document).ready(function() {
+    $('#button').click(function() {
+    printFizzBuzz($('#formValue').val());
+    });
+});
+
 function printFizzBuzz(n) {
 	if (n < 1) {
-		console.log("The number has be to larger or equal to 1");
+		console.log('The number has be to larger or equal to 1');
 		return;
 	}
+	
+	var div = document.getElementById('fizzbuzz');
+	div.innerHTML = '';
+	
 	for (i = 1; i <= n; i++) {
 		if (i%3 == 0 && i%5 == 0) {
-			console.log("FizzBuzz");
+			div.innerHTML = div.innerHTML + '<p>FizzBuzz</p>';
 		} else if (i%3 == 0) {
-			console.log("Fizz");
+			div.innerHTML = div.innerHTML + '<p>Fizz</p>';
 		} else if (i%5 == 0) {
-			console.log("Buzz");
+			div.innerHTML = div.innerHTML + '<p>Buzz</p>';
 		} else {
-			console.log(i);
+			div.innerHTML = div.innerHTML + '<p>' + i + '</p>';
 		}
 	}
 	return;
