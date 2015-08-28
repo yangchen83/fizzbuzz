7d92f1f08478561ed5245be8368c852aae87f006
Yang Chen <yang1983chen@gmail.com>
2015-08-28 16:58:49 -0400

    added code to handle n smaller than 1

--- js/assignment0.js
+++ js/assignment0.js
@@ -1,4 +1,8 @@
 function printFizzBuzz(n) {
+	if (n < 1) {
+		console.log("The number has be to larger or equal to 1");
+		return;
+	}
 	for (i = 1; i <= n; i++) {
 		if (i%3 == 0 && i%5 == 0) {
 			console.log("FizzBuzz");
