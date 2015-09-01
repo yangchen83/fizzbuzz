f54f91b176cbbc1773875a67a74fa04b3fc828ab
Yang Chen <yang1983chen@gmail.com>
2015-09-01 14:48:18 -0400

    final version

--- js/assignment0.js
+++ js/assignment0.js
@@ -1,28 +1,24 @@
 $(document).ready(function() {
     $('#button').click(function() {
-    printFizzBuzz($('#formValue').val());
+      var div = document.getElementById('fizzbuzz');
+      var n = Number($('#formValue').val());
+      if (isNaN(n)) {
+        div.innerHTML = '<p>What you entered is not a number. Please enter again!</p>';
+      } else if (n < 1) {
+        div.innerHTML = '<p>The number has be to larger or equal to 1. Please enter again!</p>';
+      } else {
+        div.innerHTML = '';
+        for (i = 1; i <= n; i++) {
+      		if (i%3 == 0 && i%5 == 0) {
+      			div.innerHTML = div.innerHTML + '<p>FizzBuzz</p>';
+      		} else if (i%3 == 0) {
+      			div.innerHTML = div.innerHTML + '<p>Fizz</p>';
+      		} else if (i%5 == 0) {
+      			div.innerHTML = div.innerHTML + '<p>Buzz</p>';
+      		} else {
+      			div.innerHTML = div.innerHTML + '<p>' + i + '</p>';
+      		}
+      	}
+      }
     });
 });
-
-function printFizzBuzz(n) {
-	if (n < 1) {
-		console.log('The number has be to larger or equal to 1');
-		return;
-	}
-	
-	var div = document.getElementById('fizzbuzz');
-	div.innerHTML = '';
-	
-	for (i = 1; i <= n; i++) {
-		if (i%3 == 0 && i%5 == 0) {
-			div.innerHTML = div.innerHTML + '<p>FizzBuzz</p>';
-		} else if (i%3 == 0) {
-			div.innerHTML = div.innerHTML + '<p>Fizz</p>';
-		} else if (i%5 == 0) {
-			div.innerHTML = div.innerHTML + '<p>Buzz</p>';
-		} else {
-			div.innerHTML = div.innerHTML + '<p>' + i + '</p>';
-		}
-	}
-	return;
-}
